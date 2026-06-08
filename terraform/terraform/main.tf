47c66999e5d09ae3eedbd9abd51081f5d056c4e8
Shapuzzz <165829371+Shapuzzz@users.noreply.github.com>
2026-06-07 15:16:52 -1000

    Add AWS provider and Minecraft security group
    Define AWS provider and create security group for Minecraft with ingress and egress rules.

---
+++ terraform/terraform/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "minecraft_sg" {
+  name = "minecraft-sg"
+
+  ingress {
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
